0c05f57f5f6a118192bdd8ba32ef9f74726d63ef
NSGOmega <70560728+NSGOmega@users.noreply.github.com>
2022-01-25 15:30:54 -0500

    No longer test only for slash commands

--- commands/vernon.js
+++ commands/vernon.js
@@ -5,7 +5,7 @@ module.exports = {
   description: 'Replies with a picture of vernon', // Required for slash commands
 
   slash: false, // Create both a slash and legacy command
-  testOnly: true, // Only register a slash command for the testing guilds
+  testOnly: false, // Only register a slash command for the testing guilds
 
   callback: ({ message, text, }) => {
 
