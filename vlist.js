abc6c97d7f9c508851795b9cf314bc1c96d48df8
NSGOmega <70560728+NSGOmega@users.noreply.github.com>
2022-01-30 15:33:00 -0500

    Update vlist.js

--- vlist.js
+++ vlist.js
@@ -16,6 +16,15 @@ function pad(n){
     return n;
 }
     
+let collection = [
+    {
+        name: "24H",
+        amount: 3,
+        link: "",
+        thumb: "",
+        worth: "",
+    }
+]
 
 let v_list = [
     { 
@@ -71,7 +80,7 @@ let v_list = [
         era: "AL1",
         quality: "Rare",
         title: "[⭐️⭐️] AL1 Alone 01 Vernon",
-        collection: 2,
+        collection: 3,
         id: pad(count())
 
     },
@@ -83,18 +92,55 @@ let v_list = [
         era: "AL1",
         quality: "Rare",
         title: "[⭐️⭐️] AL1 All 03 Vernon",
-        collection: 2,
+        collection: 3,
         id: pad(count())
 
     },
     {
         link: "http://vernonbot.xyz/vernon-img/6.jpeg",
+        thumb: "https://vernonbot.xyz/vernon-img/thumbnail/alone.jpg",
+        version: "All 13",
+        worth: 250,
+        era: "AL1",
+        quality: "Rare",
+        title: "[⭐️⭐️] AL1 All 13 Vernon",
+        collection: 3,
+        id: pad(count())
+
+    },
+    {
+        link: "http://vernonbot.xyz/vernon-img/7.jpeg",
         thumb: "https://vernonbot.xyz/vernon-img/thumbnail/anode.jpeg",
         version: "Truth - White 01",
         worth: 100,
         era: "An Ode",
         quality: "Common",
         title: "[⭐️] An Ode Truth - White 01 Vernon",
+        collection: 2,
+        id: pad(count())
+
+    },
+    {
+        link: "http://vernonbot.xyz/vernon-img/8.jpeg",
+        thumb: "https://vernonbot.xyz/vernon-img/thumbnail/anode.jpeg",
+        version: "Hope - White 01",
+        worth: 100,
+        era: "An Ode",
+        quality: "Common",
+        title: "[⭐️] An Ode Hope - White 01 Vernon",
+        collection: 2,
+        id: pad(count())
+
+    },
+    {
+        link: "http://vernonbot.xyz/vernon-img/9.jpeg",
+        thumb: "https://vernonbot.xyz/vernon-img/thumbnail/anode.jpeg",
+        version: "Begin - Color 01",
+        worth: 100,
+        era: "An Ode",
+        quality: "Common",
+        title: "[⭐️] An Ode Begin - Color 01 Vernon",
+        collection: 2,
         id: pad(count())
 
     },
